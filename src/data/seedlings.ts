e550a86c3a5466604ff492f1efc12dba5beaa6d3
iiann-dev <romoveall@gmail.com>
2026-08-22 22:15:44 +0700

    Update Google Maps embed URL for Turia Farm

--- src/data/seedlings.ts
+++ src/data/seedlings.ts
@@ -9,7 +9,7 @@ export const SITE_CONFIG = {
   address: "Turia's Farm Kediri, Batuaji, Ringinrejo, Kab. Kediri, Jawa Timur 64172, Indonesia",
   location: "Batuaji, Ringinrejo, Kab. Kediri, Jawa Timur, Indonesia",
   mapsEmbed:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.25!2d112.1038139!3d-7.966564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78f1006725ea41%3A0x2a565eacbeca6fbe!2sTURIA'S%20FARM%20KEDIRI%20%23bibitpisang%26sengon!5e0!3m2!1sid!2sid!4v1720000000000",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.3131023966703!2d112.10123897405272!3d-7.966558679400668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78f1006725ea41%3A0x2a565eacbeca6fbe!2sTURIA'S%20FARM%20KEDIRI%20%23bibitpisang%20%26%20sengon!5e0!3m2!1sen!2sid!4v1787411138372!5m2!1sen!2sid",
   mapsLink: "https://maps.app.goo.gl/3WbP6pDM9GreKdVG9",
   geo: {
     lat: -7.966564,
